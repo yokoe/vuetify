646fa19c0e2eee665e4a64d14df270ad293e244d
John Leider <john.j.leider@gmail.com>
2019-03-29 00:09:58 -0400

    docs(VGrid): fix example
    add nested grid-list

--- packages/docs/src/examples/grid/nestedGrid.vue
+++ packages/docs/src/examples/grid/nestedGrid.vue
@@ -1,45 +1,89 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-layout row wrap>
-      <v-flex d-flex xs12 sm6 md4>
-        <v-card color="purple" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
+  <v-container
+    fluid
+    grid-list-md
+  >
+    <v-layout>
+      <v-flex
+        d-flex
+        xs12
+        sm6
+        md4
+      >
+        <v-card
+          color="purple"
+          dark
+        >
+          <v-card-title
+            primary
+            class="title"
+          >Lorem</v-card-title>
           <v-card-text>{{ lorem }}</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex d-flex xs12 sm6 md3>
-        <v-layout row wrap>
-          <v-flex d-flex>
-            <v-card color="indigo" dark>
-              <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
-            </v-card>
-          </v-flex>
-          <v-flex d-flex>
-            <v-layout row wrap>
-              <v-flex
-                v-for="n in 2"
-                :key="n"
-                d-flex
-                xs12
+      <v-flex
+        d-flex
+        xs12
+        sm6
+        md3
+      >
+        <v-container grid-list-md>
+          <v-layout>
+            <v-flex
+              grow
+              shrink
+            >
+              <v-card
+                color="indigo"
+                dark
               >
-                <v-card
-                  color="red lighten-2"
-                  dark
-                >
-                  <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-        </v-layout>
+                <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex>
+              <v-container grid-list-md>
+                <v-layout>
+                  <v-flex
+                    v-for="n in 2"
+                    :key="n"
+                    xs12
+                  >
+                    <v-card
+                      color="red lighten-2"
+                      dark
+                    >
+                      <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-flex>
-      <v-flex d-flex xs12 sm6 md2 child-flex>
-        <v-card color="green lighten-2" dark>
+      <v-flex
+        d-flex
+        xs12
+        sm6
+        md2
+      >
+        <v-card
+          color="green lighten-2"
+          dark
+        >
           <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex d-flex xs12 sm6 md3>
-        <v-card color="blue lighten-2" dark>
+      <v-flex
+        d-flex
+        xs12
+        sm6
+        md3
+      >
+        <v-card
+          color="blue lighten-2"
+          dark
+        >
           <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
         </v-card>
       </v-flex>
