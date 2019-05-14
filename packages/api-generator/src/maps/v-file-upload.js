b52f913baa44deb9d703442095374d3822d4caec
MajesticPotatoe <AMajesticPotatoe@gmail.com>
2019-05-13 21:16:54 -0400

    docs(VFileUpload): add api map file

---
+++ packages/api-generator/src/maps/v-file-upload.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'v-alert': {
+    props: [
+      {
+        'name': 'accept',
+        'type': 'String',
+        'default': '*',
+        'source': null
+      },
+      {
+        'name': 'disabled',
+        'type': 'Boolean',
+        'default': 'false',
+        'source': null
+      },
+      {
+        'name': 'file-details',
+        'type': 'Boolean',
+        'default': 'false',
+        'source': null
+      },
+      {
+        'name': 'multiple',
+        'type': 'Boolean',
+        'default': 'false',
+        'source': null
+      },
+      {
+        'name': 'read-as',
+        'type': 'String',
+        'default': 'undefined',
+        'source': null
+      },
+      {
+        'name': 'upload-on-select',
+        'type': 'Boolean',
+        'default': 'false',
+        'source': null
+      },
+      {
+        'name': 'uploader',
+        'type': 'Function',
+        'default': 'undefined',
+        'source': null
+      }
+    ]
+  }
+}
+// accept, disabled, file-details, multiple, read-as, upload-on-select, uploader
